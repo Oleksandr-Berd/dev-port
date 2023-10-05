c7b4de8998f85c3211de12a84c5c136630ade476
Oleksandr-Berd <alex.berd86@gmail.com>
2023-10-05 20:15:47 +0300

    beginning

---
+++ src/components/SocNet/SocNet.tsx
@@ -0,0 +1,13 @@
+import {ReactComponent as GitHubSVG} from "../../assets/images/icon-github.svg"
+
+const SocNet:React.FC = () => {
+    return (
+      <ul>
+        <li>
+          <a href="https://github.com/Oleksandr-Berd"><GitHubSVG/></a>
+        </li>
+      </ul>
+    );
+}
+ 
+export default SocNet;
